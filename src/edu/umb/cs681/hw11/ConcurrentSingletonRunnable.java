fab7efd554805da8073290c3a0f24fa779eeb3a2
ChenYeh Chien <ChenYeh.Chien001@umb.edu>
2020-12-12 06:27:40 +0800

    CS681HW11

---
+++ src/edu/umb/cs681/hw11/ConcurrentSingletonRunnable.java
@@ -0,0 +1,20 @@
+package edu.umb.cs681.hw11;
+
+public class ConcurrentSingletonRunnable implements Runnable {
+
+	public void run() {
+		try{
+			for(int i = 0; i < 10; i++){
+				System.out.println(ConcurrentSingleton.getInstance());
+				Thread.sleep(2);
+			}
+		}catch(InterruptedException exception){}
+	}
+	
+	public static void main(String[] args) {
+		new Thread(new ConcurrentSingletonRunnable()).start(); //1
+		new Thread(new ConcurrentSingletonRunnable()).start(); //2
+		new Thread(new ConcurrentSingletonRunnable()).start(); //3
+	}
+	
+}
